0847d8e9494940bebac7fcff3e42d81d8a72ffa8
alemanjosh7 <josuealeman2003@gmail.com>
2021-01-19 22:38:30 -0600

    Segment Tree Builded in n time

--- Algorithms/SegmentTree.cpp
+++ Algorithms/SegmentTree.cpp
@@ -1,13 +1,6 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-
-#include<bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
-using namespace __gnu_pbds;
-using namespace std;
  
 typedef long long ll;
 typedef uint64_t u64;
@@ -42,41 +35,6 @@ typedef vector<vpi> wgraf;
 #define salto "\n"
 #define msz(a) memset(a, 0, sizeof a);
 
-template <typename T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-
-template <typename T>
-using VT = vector<T>;
-
-void _read(int &x){ scanf("%d", &x); }
-void _read(ll &x){ scanf("%lld", &x); }
-void _read(char *x){ scanf("%s", x); }
-void _read(char &x){ scanf(" %c", &x); }
-void _read(double& x){ scanf("%lf", &x); } 
-void _read(string& x){ cin >> x; }         
- 
-template <typename T, typename U>
-void _read(pair<T,U>& p){
-	_read(p.fst); _read(p.snd);
-}
- 
-template <typename T>
-void _read(VT<T> &V){
-	for(int i = 0; i < sz(V); i++) _read(V[i]);
-}
- 
-void read() {}
-template <typename T, typename... U>
-void read(T &head, U &... tail){
-	_read(head);
-	read(tail...);
-}
- 
-template <typename T>
-void reada(T *arr, int n){
-	for(int i = 0; i < n; i++) _read(arr[i]);
-}
-
 void setIO(string name = "") { 
     ios_base::sync_with_stdio(0); cin.tie(0); 
     if(sz(name)){
@@ -93,6 +51,23 @@ struct segtree{
 			size <<= 1;
 		}
 		sums.assign(size << 1, 0LL);
+	}
+	
+	void build(vi &a, int x, int lx, int rx){
+		if(rx - lx == 1){
+			if(lx < sz(a)){
+				sums[x] = a[lx];
+			}
+			return;
+		}
+		int m = (lx + rx) / 2;
+		build(a, 2 * x + 1, lx, m);
+		build(a, 2 * x + 2, m, rx);
+		sums[x] = sums[2 * x + 1] + sums[2 * x + 2];
+	}
+	
+	void build(vi &a){
+		build(a, 0, 0, size);
 	}
 	
 	void set(int i, int v, int x, int lx, int rx){
@@ -143,11 +118,13 @@ void solve(){
 	
 	st.init(n);
 	
+	vi a(n);
+	
 	for(int i = 0; i < n; i++){
-		int v;
-		cin >> v;
-		st.set(i, v);
+		cin >> a[i];
 	}
+	
+	st.build(a);
 	
 	for(int i = 0; i < m; i++){
 		int op;
